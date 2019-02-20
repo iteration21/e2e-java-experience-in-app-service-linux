db9707c0648d7878aefdf73b9106d400f3b272bf
Dariusz Boberek <dariusz.boberek@vintom.com>
2019-02-20 17:18:58 +0100

    remove --recurse-submodules for real

---
+++ i21_initial-hsqldb/spring-framework-petclinic/set-env-variables-template.sh
@@ -0,0 +1,10 @@
+# Tomcat Info
+
+export TOMCAT_HOME=<my-tomcat-home-directory>
+
+# Azure Environment
+
+export RESOURCEGROUP_NAME=<my-resource-group-name>
+export WEBAPP_NAME=<my-webapp-name>
+export WEBAPP_PLAN_NAME=${WEBAPP_NAME}-appservice-plan
+export REGION=<my-region>
